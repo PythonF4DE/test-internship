167016b94031b7563820a543b1d27f7f8d3269b3
Riot <romanmissuryn@gmail.com>
2026-06-19 16:24:37 +0300

    awdasdw

--- Task1-RM/mysql/setup.sql
+++ Task1-RM/mysql/setup.sql
@@ -1,4 +1,4 @@
--- example values, do not commit actual values to GitHub22
+-- example values, do not commit actual values to GitHub22w
 CREATE DATABASE example_db;
 CREATE USER 'example_user'@'0.0.0.0' IDENTIFIED BY 'example_password';
 GRANT ALL PRIVILEGES ON example_db.* TO 'example_user'@'0.0.0.0';
