da6c80992b3b3d49c08b693f1d5c361145ef78d0
Riot <romanmissuryn@gmail.com>
2026-06-19 16:03:00 +0300

    awdasdw

--- Task1-RM/websites/website-2/app.py
+++ Task1-RM/websites/website-2/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', image_file='bigb222222ird.jpg')
+    return render_template('index.html', image_file='bigb22222s2ird.jpg')
 
 
 if __name__ == '__main__':
